266d7fd3e10876cecfbb0230357b036f40b6e259
Anshul Dod <anshuldod4@gmail.com>
2026-07-30 23:38:06 +0530

    Time: 1 ms (97.96%) | Memory: 42.8 MB (25.74%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -7,14 +7,25 @@ class Solution {
 
         // }
         // return num;
+        // more optimeal
+        // if(num==0){
+        //     return 0;
+        // }else if(num%9==0 && num>0){
+        //     return 9;
+        // }else{
+        //     // not disvisoble by 9 then give remainder
+        //     int n=num%9;
+        //     return n;
+        // }
+
+
+
+        // exact omptimal
         if(num==0){
             return 0;
-        }else if(num%9==0 && num>0){
-            return 9;
-        }else{
-            // not disvisoble by 9 then give remainder
-            int n=num%9;
-            return n;
         }
+
+        return 1 + (num-1)%9;
+
     }
 }
