8274093d4971b7bc9be19c4d48d97e5be23767af
Anshul Dod <anshuldod4@gmail.com>
2026-08-07 08:30:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (51.49%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isHappy(int n) {
+       
+        while(n>9 && n!= 7){
+            int sum =0;
+            while(n>0){
+                int digit =n %10;
+                sum+=digit *digit;
+                n/=10;
+            }
+            n=sum;
+        }
+        return n==1 || n==7;
+    }
+}
